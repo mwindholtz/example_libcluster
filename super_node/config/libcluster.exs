fdf8e378bd7591e7258130be487d634934da7a38
Mark Windholtz <mark@agiledna.com>
2018-10-30 09:14:43 -0400

    adds libcluster

---
+++ super_node/config/libcluster.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+config :libcluster,
+  topologies: [
+    exploring_elixir: [
+      strategy: Cluster.Strategy.Gossip,
+      # config: {},
+      connect: {ExploringElixir.AutoCluster, :connect_node, []},
+      disconnect: {ExploringElixir.AutoCluster, :disconnect_node, []}
+      # list_nodes: {:erlang, :nodes, [:connected]},
+      # child_spec: [restart: :transient]
+    ]
+  ]
